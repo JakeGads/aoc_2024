01f4765944b93e12cc6a4ae6254cda7bc1060b9f
Jake Gads <jake.gads@gmail.com>
2024-12-01 12:48:55 -0500

    end of day 1 changes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod file_manager;
-mod day1;
 
 fn main() {
     let x = file_manager::read("inputs/day1".to_string(), None);
-    day1::part1(x.clone());
-    day1::part2(x.clone());
+    println!("{:?}", x);
 }
